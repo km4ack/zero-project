f88a43688fb4ba7824c227aec78bf8b4c462bf86
KM4ACK <jason.oleham@gmail.com>
2020-09-09 10:26:40 -0500

    require login

--- php/restartpat.php
+++ php/restartpat.php
@@ -1,4 +1,10 @@
 <?php
+
+session_start();
+    if(!isset($_SESSION['login'])) {
+        header('LOCATION:/control/login.php'); die();
+    }
+
 $output = shell_exec('/usr/local/bin/restartpat');
 echo "<pre>$output</pre>";
 header('Location: /control.php?success=true');
