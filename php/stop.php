46e0b593d1392108801a8cf6ec0b78e40e8ce450
KM4ACK <jason.oleham@gmail.com>
2020-09-09 10:27:10 -0500

    require login

--- php/stop.php
+++ php/stop.php
@@ -1,4 +1,10 @@
 <?php
+
+session_start();
+    if(!isset($_SESSION['login'])) {
+        header('LOCATION:/control/login.php'); die();
+    }
+
 $output = shell_exec('/usr/local/bin/removeblue');
 echo "<pre>$output</pre>";
 header('Location: /control.php?success=true');
